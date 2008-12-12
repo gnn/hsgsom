692ff26ab3ca65c87a0cdea58ef7131462e35e67
gnn.github@gmail.com <unknown>
2008-12-12 08:56:13 +0000

    Implementation: wrote a main stub.

---
+++ main.hs
@@ -0,0 +1,12 @@
+module Main where 
+
+import System.Environment(getArgs)
+
+import Gsom.Gsom
+
+main :: IO ()
+main = do 
+  arguments <- getArgs
+  let input = if null arguments then "input.txt" else head arguments
+  return ()
+
