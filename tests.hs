cc12d92a7fe43e6e806944ba2b7968acdc3b95c4
gnn.github@gmail.com <unknown>
2008-12-24 03:52:35 +0000

    Added tests.hs script along with some tests. There now is a tests.hs script which can be used to run the tests. It currently contains two tests namely normalized is tested for beeing idempotent and for adhering to the bounds of 0 and 1.

---
+++ tests.hs
@@ -0,0 +1,51 @@
+#! /usr/bin/env runhaskell
+-- | Just tests.
+
+module Main where 
+
+import Data.List(transpose)
+import Prelude hiding (Either(..))
+
+import Test.QuickCheck
+
+-- Modules which are only imported so that functions of them can be tested.
+import Gsom(normalize)
+
+data Alignment = Left | Right
+
+-- | @'format' alignment separator table@ treats each element of table as 
+-- one line and each string in each line as column. Columns are then 
+-- formatted to be aligned according to alignment and separated by separator.
+-- Each column is made large enough to fit its largest element.
+format :: [[String]] -> Alignment -> String -> [[String]]
+format table alignment separator = map (zipWith pad lengths) table where
+  lengths = map (foldr (max.length) 0) (transpose table)
+  pad n s = (case alignment of 
+    Left  -> (s ++  )
+    Right -> (  ++ s)
+    $  replicate (n - length s) ' ' ) ++ separator
+
+-- | @'formatLabels' labeled_list@ formats the labels in the given list
+-- so that the label column is big enough for every label, the labels are
+-- left aligned and there is a colob before each test result.
+formatLabels :: [(String, a)] -> [(String, a)]
+formatLabels ps = zip formatted (map snd ps) where 
+  formatted = map head $ format table Left ": "
+  table = map ((:[]).fst) ps
+
+main :: IO ()
+main = mapM_ (\(label, test) -> putStr label >> test) tests
+
+-- | All the tests which should be performed along with their labels.
+tests :: [(String, IO ())]
+tests = formatLabels 
+  $ ("normalize/idempotent", test prop_normalize_idempotent)
+  : ("normalize/bounds", test prop_normalize_bounds)
+  : []
+
+-- Normalize should be idempotent.
+prop_normalize_idempotent s = (normalize.normalize) s == normalize s
+
+-- The result of normalize should contain only values between 0 and 1.
+prop_normalize_bounds s = and.(map and).(map $ map test) $ (normalize s) where
+ test = (\x -> (x>=0 && x<=1))
