d82cc63ce0df9cd7fa8b05c4d17aae35cc304b91
gnn.github@gmail.com <unknown>
2008-12-28 09:37:34 +0000

    Added some arithmetic functions to type Input.

--- gsom/input.hs
+++ gsom/input.hs
@@ -27,3 +27,25 @@ dimension = maximum . map length
 distance :: Input -> Input -> Double
 distance i1 i2 = sqrt $ sum $ zipWith (\a b -> (a-b)**2) i1 i2
 
+
+-- | Multiplication of an input vector with a scalar.
+infixr 7 .* 
+(.*) :: Double -> Input -> Input
+(.*) d = map $ \x -> d*x
+
+infixl 7 *.
+(*.) :: Input -> Double -> Input
+(*.) = flip (.*)
+
+-- | Subtraction and addition of vectors between each other.
+infixl 6 <->, <+>
+
+(<+>), (<->) :: Input -> Input -> Input
+(<+>) i1 i2 = let 
+  front = zipWith (+) i1 i2 
+  l1 = length i1
+  l2 = length i2 in case signum $ l1 - l2 of
+    0   -> front
+    -1  -> front ++ drop l1 i2
+    1   -> front ++ drop l2 i1
+(<->) i1 i2 = i1 <+> (-1) .* i2
