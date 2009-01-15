f3854bb69136ef658154cfaaa36301a1bfb080bb
gnn.github@gmail.com <unknown>
2009-01-15 22:29:28 +0000

    Refactored parameters for Gsom. Factored parameters which are constant over the running time of the algorithm out and bundled them up into an own datatype. This also led to the removal of them random generator as parameter of Gsom. It is more of a paramter to the creation of the initial lattice and there it may as well be passed explicitly.

--- gsom.hs
+++ gsom.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE ExistentialQuantification #-}
-
 module Gsom(Gsom(..), Parameters(..), Input, Inputs) where
 
 import System.Random
@@ -7,16 +5,24 @@ import System.Random
 import Gsom.Lattice(Lattice)
 import Gsom.Input(Input, Inputs)
 
--- | These are the parameters needed to create a growing self organizing map.
-data Parameters = forall g. (RandomGen g) => Parameters {
-  -- | The input as well as the weight vector dimension. This is needed so 
+-- | These are the parameters needed to create a gsom which are needed in every 
+-- iteration and do not change while the algorithm runs.
+data Parameters = Parameters {
+  -- | The alpha value is used to gradually reduce the learning rate in each 
+  -- iteration of the GSOM algorithm. Reduction is done according to the 
+  -- formula:
+  --
+  -- * @newLearningRate = alpha * f(n) * oldLearningRate@
+  -- * @f(n) = (1-R/n)
+  --
+  -- where @n@ is the number of nodes currently in the map and @R@ is 
+  -- currently taken to be @3.8@, as specified in the paper by Alahakoon and 
+  -- Halgamuge. 
+  alpha         :: Double
+, -- | The input as well as the weight vector dimension. This is needed so 
   -- that the growth threshhold may be calculated.
   d             :: Int
-, -- | The random number generator use whenever we need random values. I think 
-  -- those are only needed during grid initialisation so maybe they may be 
-  -- dropped
-  randomizer    :: g
-, -- | The spread factor is used to calculate the growth threshhold according 
+, -- | The spread factor is used to calculate the growth threshold according 
   -- to the formula GT = - sqrt('d')*ln('spreadFactor').
   spreadFactor  :: Double
 }
